8b22c99784f5613798371b677002748420409b90
Titov Sergei <quosapv@yandex.ru>
2017-12-04 01:13:58 +0300

    PostProxyInvokerContextListener now works

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -2,8 +2,6 @@ package quoters;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static java.lang.Thread.sleep;
-
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
--- src/main/java/quoters/annotations/PostProxy.java
+++ src/main/java/quoters/annotations/PostProxy.java
@@ -1,7 +1,11 @@
 package quoters.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * @author Tit on 03.12.2017
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PostProxy {
 }
